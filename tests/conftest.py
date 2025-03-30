9d250c9240a37ed230f8d8b4d37d614c6bef5d13
Elias Gabriel <me@eliasfgabriel.com>
2025-03-30 02:08:28 -0400

    fix: windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,20 @@
+import sys
+
 import pytest
 
 
 @pytest.fixture(
     params=[
         pytest.param(("asyncio", {"use_uvloop": False}), id="asyncio"),
-        pytest.param(("asyncio", {"use_uvloop": True}), id="uvloop"),
+        pytest.param(
+            ("asyncio", {"use_uvloop": True}),
+            id="uvloop",
+            marks=[
+                pytest.mark.skipif(
+                    sys.platform == "win32", reason="uvloop not available"
+                ),
+            ],
+        ),
         pytest.param(
             ("trio", {"restrict_keyboard_interrupt_to_checkpoints": True}), id="trio"
         ),
